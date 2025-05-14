5dc169809ab36c352b0bece2336991e401a6179a
Nataliage <nataliagemb@gmail.com>
2025-05-14 19:26:33 +0200

    añadido api para todos los endpoints

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+# Cargar variables de entorno desde el archivo .env
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.auth.routes import router as auth_router
@@ -7,7 +10,7 @@ from app.game.routes import router as game_router
 #from app.config.firebase import firebase_app
 
 
-app = FastAPI(title="DevQuest API", description="Backend API for DevQuest application", version= "1.0.0")
+app = FastAPI(title="DevQuest API", description="Backend API for DevQuest application", version= "1.0.0", openapi_prefix= "/api", docs_url="/docs", redoc_url=None, openapi_url="/openapi.json")
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,13 +20,18 @@ app.add_middleware(
     allow_headers=["Authorization", "Content-Type"],
 )
 #registrar los routers de cada modulo de la aplicacion
-app.include_router(auth_router)
-app.include_router(levels_router)
-app.include_router(progress_router)
-app.include_router(game_router)
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(levels_router, prefix="/levels", tags=["Levels"])
+app.include_router(progress_router, prefix="/progress", tags=["Progress"])
+app.include_router(game_router, prefix="/game", tags=["Game"])
+
+#app.include_router(auth_router)   
+#app.include_router(levels_router)
+#app.include_router(progress_router)
+#app.include_router(game_router)
 
 #endpoint raíz para verificar que la API funciona
-@app.get("/")
+@app.get("/api")
 def read_root():
     """
     Endpoint raíz que confirma que la API está en funcionamiento.
@@ -31,5 +39,5 @@ def read_root():
     """    
     return {"message": "Bienvenido a la API de DevQuest",
             "version": "1.0.0",
-            "docs": "/docs"
+            "docs": "api/docs"
             }
